6589a24fdd39e4c9864a833510a8c01b3067a2ca
ShadXo <gold_guver@hotmail.com>
2021-07-15 22:28:58 +0200

    Update to new version

--- update_wallet.sh
+++ update_wallet.sh
@@ -2,7 +2,7 @@ echo
 echo "MONK - Masternode updater"
 echo ""
 echo "Welcome to the MONK Masternode update script."
-echo "Wallet v3.0.0.0"
+echo "Wallet v3.0.1.1"
 echo
 
 for filename in ~/bin/monk-cli*.sh; do
@@ -17,14 +17,14 @@ cd
 
 mkdir -p MONK_TMP
 cd MONK_TMP
-wget https://github.com/decenomy/MONK/releases/download/v3.0.0.0/MONK-3.0.0.0-Linux.zip
-sudo chmod 775 MONK-3.0.0.0-Linux.zip
+wget https://github.com/decenomy/MONK/releases/download/v3.0.1.1/MONK-3.0.1.1-Linux.zip
+sudo chmod 775 MONK-3.0.1.1-Linux.zip
 #tar -xvzf monkey-2.3.0-x86_64-linux-gnu.tar.gz
-unzip MONK-3.0.0.0-Linux.zip -d MONK-3.0.0.0
+unzip MONK-3.0.1.1-Linux.zip -d MONK-3.0.1.1
 
-rm -f MONK-3.0.0.0-Linux.zip
-sudo chmod 775 ./MONK-3.0.0.0/bin/*
-sudo mv ./MONK-3.0.0.0/bin/monk* /usr/bin
+rm -f MONK-3.0.1.1-Linux.zip
+sudo chmod 775 ./MONK-3.0.1.1/*
+sudo mv ./MONK-3.0.1.1/monk* /usr/bin
 
 cd ~
 rm -rdf MONK_TMP
