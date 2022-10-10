957128581f1a51a81af97e2b4710b1ccf0911d2d
ShadXo <gold_guver@hotmail.com>
2022-10-10 21:01:54 +0200

    Minor fixes in fork finder

--- find_fork.sh
+++ find_fork.sh
@@ -20,28 +20,34 @@ do
 
 	for (( ; ; ))
 	do
-		echo "Starting from $BLOCK"
+    echo "Running check on $WALLET"
+    echo "Starting from block $BLOCK"
 
 		for FILE in $(ls ~/bin/${NAME}-cli_$WALLET.sh | sort -V); do
       echo "*******************************************"
-			echo $FILE
-			BLOCKHASHEXPLORER=$(curl -s4 https://explorer.decenomy.net/coreapi/v1/coins/MONK/blocks | jq -r ".response[0].blockhash")
-			BLOCKHASHWALLET=$($FILE getblockhash $BLOCK)
+      echo "Checking block $BLOCK"
+			#echo $FILE
 
+			BLOCKHASHEXPLORER=$(curl -s4 https://explorer.decenomy.net/coreapi/v1/coins/MONK/blocks | jq -r ".response[0].blockhash")
 			if [ -z "$BLOCKHASHEXPLORER" ]; then
 			  break 2
 			fi
-
-			if [ -z "$BLOCKHASHWALLET" ]; then
+      if [ "$BLOCKHASHEXPLORER" == null ]; then
+        echo "NO FORK FOUND"
 			  break 2
 			fi
 
-			echo "BLOCKHASHEXPLORER=$BLOCKHASHEXPLORER"
-			echo "BLOCKHASHWALLET=$BLOCKHASHWALLET"
+      BLOCKHASHWALLET=$($FILE getblockhash $BLOCK)
+      if [ -z "$BLOCKHASHWALLET" ]; then
+			  break 2
+			fi
+
+      echo "BLOCKHASHEXPLORER=$BLOCKHASHEXPLORER"
+      echo "BLOCKHASHWALLET=$BLOCKHASHWALLET"
 		done
 
-		if [ "$BLOCKHASHWALLET" != "$BLOCKHASHEXPLORER" ]; then
-		  echo "FORK ON $BLOCK !!!!"
+    if [ "$BLOCKHASHWALLET" != "$BLOCKHASHEXPLORER" ]; then
+		  echo "FORK FOUND ON BLOCK $BLOCK !!!!"
 		  break
 		fi
 
